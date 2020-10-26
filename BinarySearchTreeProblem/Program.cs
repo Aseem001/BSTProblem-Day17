2b06581c2c0934d27ff5a5cdb4e66752849fdbfa
Aseem <aseemanand2@gmail.com>
2020-10-27 00:33:30 +0530

    [Aseem] Added code to search for an element into the BST

--- BinarySearchTreeProblem/Program.cs
+++ BinarySearchTreeProblem/Program.cs
@@ -25,7 +25,9 @@ namespace BinarySearchTreeProblem
             binarySearch.Insert(16);
             binarySearch.Insert(63);
             binarySearch.Insert(67);           
-            binarySearch.GetSizeAndDisplay();          
+            binarySearch.GetSizeAndDisplay();
+            Console.WriteLine("Is the element present: "+binarySearch.IfExists(63, binarySearch));
+            Console.ReadLine();
         }
     }
 }
