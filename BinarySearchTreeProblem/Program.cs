1c3dddf36808bc585e7feeaa23256947c5a2101f
Aseem <aseemanand2@gmail.com>
2020-10-26 23:52:41 +0530

    [Aseem] Added code to find the size of BST and display its elements

--- BinarySearchTreeProblem/Program.cs
+++ BinarySearchTreeProblem/Program.cs
@@ -1,15 +1,31 @@
-﻿using System;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Aseem Anand"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace BinarySearchTreeProblem
 {
+    using System;
+
     class Program
     {
         static void Main(string[] args)
         {
             BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
             binarySearch.Insert(30);
-            binarySearch.Insert(70);                      
-            binarySearch.Display();                        
+            binarySearch.Insert(70);
+            binarySearch.Insert(22);
+            binarySearch.Insert(40);
+            binarySearch.Insert(60);
+            binarySearch.Insert(95);
+            binarySearch.Insert(11);
+            binarySearch.Insert(65);
+            binarySearch.Insert(3);
+            binarySearch.Insert(16);
+            binarySearch.Insert(63);
+            binarySearch.Insert(67);           
+            binarySearch.GetSizeAndDisplay();          
         }
     }
 }
